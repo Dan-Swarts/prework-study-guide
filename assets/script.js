c3b1922a5c2cf952817b4dbde72df4bd94fb6501
Daniel Swarts <swart179@umn.edu>
2024-07-25 16:48:19 -0500

    added javascript

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+const topics = ["HTML","CSS","Git","JavaScript"];
+
+let topic = "HTML";
+
+// if(topic == "HTML"){
+//     console.log("Let's study HTML!");
+// }
+
+// else if (topic == "CSS"){
+//     console.log("Let's study CSS");
+// }
+
+// else if (topic == "Git"){
+//     console.log("Let's study Git");
+// }
+
+// else if (topic == "Javascript"){
+//     console.log("Let's study JavaScript");
+// }
+
+for(let x = 0; x < topics.length; x++){
+    console.log(topics[x]);
+}
+
+// const shapes = ["triangle", "circle", "square", "rectangle"];
+
+// for(let x =0; x <shapes.length; x++){
+//     console.log(shapes[x]);
+// }
